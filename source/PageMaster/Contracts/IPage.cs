93e797af43d31a2fcfe494f8b18246be882eed35
Lars Krämer <lars.kraemer@live.de>
2024-10-27 22:58:39 +0100

    PM-10: update Page

--- source/PageMaster/Contracts/IPage.cs
+++ source/PageMaster/Contracts/IPage.cs
@@ -5,29 +5,29 @@ public interface IPage<T>
     /// <summary>
     /// Current page number.
     /// </summary>
-    ulong CurrentPage { get; init; }
+    ulong CurrentPage { get; }
     /// <summary>
     /// Maximum number of entries returned per page.
     /// </summary>
-    ulong PerPage { get; init; }
+    ulong PageSize { get; }
     /// <summary>
     /// Total number of entries that exist for this query. Can be null if unknown.
     /// </summary>
-    ulong? TotalEntries { get; init; }
+    ulong TotalCount { get; }
     /// <summary>
     /// The items of this page.
     /// </summary>
-    IEnumerable<T> Items { get; init; }
+    IEnumerable<T> Items { get; }
     /// <summary>
     /// Page number of the last page available. Can be null if the current page is the last one.
     /// </summary>
-    ulong? LastPage { get; init; }
+    ulong LastPage { get; }
     /// <summary>
     /// Page number of the next page. Can be null if the current page is the last one.
     /// </summary>
-    ulong? NextPage { get; init; }
+    ulong? NextPage { get; }
     /// <summary>
     /// Page number of the previous page. Can be null if the current page is the first one.
     /// </summary>
-    ulong? PreviousPage { get; init; }
+    ulong? PreviousPage { get; }
 }
